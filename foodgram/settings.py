4c0063cec52de682d3da8f255115c5958cf6453e
Сергей <kofan1106@gmail.com>
2021-03-07 16:04:56 +0300

    Making a graduation project v.0.16

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
